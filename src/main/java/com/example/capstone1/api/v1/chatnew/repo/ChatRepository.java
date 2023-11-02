ea866170406f5a5b9d7032f1956dcf4b41099a15
treejh <jihyun3066@naver.com>
2023-11-02 23:30:37 +0900

    chat Si

--- src/main/java/com/example/capstone1/api/v1/chatnew/repo/ChatRepository.java
+++ src/main/java/com/example/capstone1/api/v1/chatnew/repo/ChatRepository.java
@@ -4,19 +4,21 @@ import com.example.capstone1.api.entity.Follow;
 import com.example.capstone1.api.v1.chatnew.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    @Query("select b from Chat b where b.users.username=:username")
-    List<Chat> findChatByUserName(String username);
+    @Query("select b from Chat b where b.users.username = :username")
+    List<Chat> findChatByUserName(@Param("username") String username);
 
-    @Query("select b from Chat b where b.chatRoom.id=:roomId")
-    Optional<List<Chat>> findChatByRoomId(long roomId);
+    @Query("select b from Chat b where b.chatRoom.id = :roomId")
+    Optional<List<Chat>> findChatByRoomId(@Param("roomId") long roomId);
 
-    @Query("SELECT b FROM Chat b WHERE b.users.username=:username AND b.chatRoom.id=:roomId")
-    List<Chat> findChatByUserNameAndRoomId(String username,long roomId);
+    @Query("SELECT b FROM Chat b WHERE b.users.username = :username AND b.chatRoom.id = :roomId")
+    List<Chat> findChatByUserNameAndRoomId(@Param("username") String username, @Param("roomId") long roomId);
 
 
 
--- src/main/java/com/example/capstone1/api/v1/chatnew/repo/ChatRoomRepository.java
+++ src/main/java/com/example/capstone1/api/v1/chatnew/repo/ChatRoomRepository.java
@@ -2,11 +2,14 @@ package com.example.capstone1.api.v1.chatnew.repo;
 
 import com.example.capstone1.api.entity.BaseTime;
 
+import com.example.capstone1.api.entity.BookMark;
 import com.example.capstone1.api.entity.Follow;
 import com.example.capstone1.api.v1.chatnew.entity.Chat;
 import com.example.capstone1.api.v1.chatnew.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +18,11 @@ import java.util.Optional;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     @Query("select b from ChatRoom b where b.id=:roomId")
-    ChatRoom findChatRoomByRoomId(long roomId);
+    ChatRoom findChatRoomByRoomId(@Param("roomId") long roomId);
+
+    @Query("select b from ChatRoom b where b.users.username=:username")
+    List<ChatRoom> findChatRoomByUsername(@Param("username") String username);
+
+
+
 }
